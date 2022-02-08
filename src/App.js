47d684149518381220bde09c2f6ff07d64905dfa
eduardodallatorre <eduardodallatorre.io@gmail.com>
2022-02-08 22:25:05 +0000

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [weight, setWeight] = useState('')
+  const [height, setHeight] = useState('')
+  const [message, setMessage] = useState('')
+
+function calculateBMI() {
+  const hei = height / 100
+  const bmi = weight / (hei * hei)
+
+  if (bmi < 18.6) {
+    setMessage("You're underweight! Your BMI: " + bmi.toFixed(2))
+  } else if (bmi >= 18.6 && bmi < 24.9) {
+    setMessage("Ideal weight! Your BMI: " + bmi.toFixed(2))
+  } else if (bmi >= 24.9 && bmi < 34.9) {
+    setMessage("You are slightly overweight! Your BMI: " + bmi.toFixed(2))
+  } else if (bmi > 34.9) {
+    setMessage("Be careful you are overweight! Your BMI: " + bmi.toFixed(2))
+  }
 }
 
-export default App;
+  return (
+    <div className="app">
+      <h1>BMI - Body Mass Index</h1>
+      <span>You can calculate your BMI</span>
+
+      <div className="area-input">
+        <input 
+          type="text" 
+          placeholder="Weight in (kg) e.g. 86" 
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}          
+        />
+        <input 
+          type="text"
+          placeholder="Height in (cm) e.g. 182"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+        <button onClick={calculateBMI}>
+           Calculate
+        </button>
+
+        <h2>{message}</h2>
+      </div>
+    </div>
+  )
+}
